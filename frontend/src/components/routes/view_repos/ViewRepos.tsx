66404e4a15e3dda47a657637daf13c5c7750e117
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-01-31 17:29:02 +0000

    Added basic list of repos to application.

--- frontend/src/components/routes/view_repos/ViewRepos.tsx
+++ frontend/src/components/routes/view_repos/ViewRepos.tsx
@@ -1,1 +1,20 @@
 import * as React from "react"
+import {Button, List} from "antd"
+
+interface Props {
+    data: string[]
+}
+
+const ViewRepos = (props: Props) => {
+    return <div>
+        <List
+            size="large"
+            dataSource={props.data}
+            renderItem={item => <List.Item>
+                <Button type="primary">{item}</Button>
+            </List.Item>}
+        />
+    </div>
+};
+
+export default ViewRepos
