517e5b4be94e1c57e357918a30bdc6029a3829b1
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-01-31 16:56:25 +0000

    Added typescript support and ant design library.

---
+++ frontend/src/components/routes/home/Home.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import useStyles from "./style"
+// import coding from "../../../assets/coding.gif"
+// import Grid from "@material-ui/core/Grid/index"
+
+const Home = () => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      Testing
+      {/*<Grid container justify="center" className={classes.gridContainer}>*/}
+      {/*  <Grid item xs={12}>*/}
+      {/*    <div style={{ textAlign: "center" }}>*/}
+      {/*      <img className={classes.image} src={coding} alt="ncvi" />*/}
+      {/*    </div>*/}
+      {/*  </Grid>*/}
+      {/*</Grid>*/}
+    </div>
+  )
+}
+
+export default Home
