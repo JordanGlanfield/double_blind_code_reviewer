667aabd2da17ac706fd47942c1431988a61a2196
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-02-02 11:11:51 +0000

    Small naming adjustment in home page.

--- frontend/src/components/routes/home/Home.tsx
+++ frontend/src/components/routes/home/Home.tsx
@@ -1,16 +1,19 @@
 import * as React from "react"
 import useStyles from "./style"
 import ViewRepos from "../view_repos/ViewRepos";
+import { Typography } from "antd";
 // import coding from "../../../assets/coding.gif"
 // import Grid from "@material-ui/core/Grid/index"
+
+const { Title } = Typography;
 
 const Home = () => {
   const classes = useStyles()
 
   return (
     <div>
-      Testing
-      <ViewRepos data={["gson", "banter_repo", "linux_kernel"]}/>
+        <Title level={2}>Available repositories:</Title>
+        <ViewRepos data={["gson", "banter_repo", "linux_kernel"]}/>
     </div>
   )
 }
