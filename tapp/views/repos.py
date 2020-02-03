7a287c3bfcc3a26962f5a1d5e32f9c1019cde44d
My Name <myemail@email.com>
2020-02-03 11:54:54 +0000

    Added skeleton repos API with fake repo.

---
+++ tapp/views/repos.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, jsonify, abort
+
+bp = Blueprint("repos", __name__, url_prefix="/api/v1.0/repos")
+
+repos = {
+    0: {
+        "name": "gson",
+        "files": ["log.txt", "test.txt", "oh_no.txt"],
+        "directories": {
+            "build": {
+                "files": ["build_process.md", "build_banter.docx", "build_results.txt"],
+                "directories": {
+                    "issues": {
+                        "files": ["broken_pipeline.txt", "errors.txt"],
+                        "directories": []
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@bp.route("/view/<int:repo_id>/", defaults={"path": ""})
+@bp.route("/view/<int:repo_id>/<path:path>", methods=["GET"])
+def get_repo(repo_id: int, path: str):
+    if not repo_id in repos:
+        abort(404)
+
+    dir_contents = repos[repo_id]
+
+    if not path == "":
+        dirs = path.split("/")
+
+        for dir in dirs:
+
+            if not dir in dir_contents["directories"]:
+                abort(404)
+
+            dir_contents = dir_contents["directories"][dir]
+
+    return jsonify(flatten_directories(dir_contents))
+
+
+def flatten_directories(dir_contents):
+    flattened = {"directories": []}
+
+    for dir in dir_contents["directories"]:
+        flattened["directories"].append(dir)
+
+    flattened["files"] = dir_contents["files"][:]
+
+    return flattened
