16bfdf1d4ae27f5cc49439d37f91e4a817406742
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-02-04 17:15:51 +0000

    Implemented file view and extracted shared repo utils.

---
+++ frontend/src/utils/repoApi.ts
@@ -0,0 +1,18 @@
+async function get(uriSuffix: string) {
+  const requestOptions = {
+    method: "GET"
+  };
+
+  const response = await fetch("/api/v1.0/repos/view" + uriSuffix);
+
+  if (response.ok) {
+    return await response.json();
+  }
+
+  throw response
+}
+
+export function getFile(repoId: string, path: string) {
+  // TODO - safety of using path here?
+  return get("/" + repoId + "/" + path)
+}
