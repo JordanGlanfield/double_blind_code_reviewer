8c3160a958d3e641c839217834b976da4c9d9272
My Name <myemail@email.com>
2020-02-06 18:04:11 +0000

    Added styled-components library and added top bar with home button.

--- frontend/src/components/mainLayout/TopBar/TopBar.tsx
+++ frontend/src/components/mainLayout/TopBar/TopBar.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+
+import { Affix, Button } from "antd";
+import styled from "styled-components";
+
+import routes from "../../../constants/routes";
 
 interface Props {
 
 }
 
 const TopBar = (props: Props) => {
-  return <div>
+  const {user} = useParams();
 
-  </div>
+  return <Affix offsetTop={0}>
+    <Button type={"primary"} href={routes.getHome(user as string)}>Home</Button>
+  </Affix>
 };
 
 export default TopBar;
