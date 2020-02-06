71428e1fb3553b2baee46bae962c00a3809d2fa4
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-02-06 15:42:50 +0000

    Tested more route util functions.

--- frontend/src/utils/routeUtil.spec.ts
+++ frontend/src/utils/routeUtil.spec.ts
@@ -1,16 +1,38 @@
-import { extractPath } from "./routeUtil";
+import { extractPath, getNextDirUp } from "./routeUtil";
 
 const fullPath = "logan/repo/gson/build/issues";
 const prefix = "logan/repo/gson/";
 
-test('can extract a path', () => {
+test("can extract a path", () => {
   const result = extractPath(prefix, fullPath);
 
   expect(result).toEqual("build/issues")
 });
 
-test('extracting an empty path gives empty string', () => {
+test("extracting an empty path gives empty string", () => {
   const result = extractPath(prefix, prefix);
+
+  expect(result).toEqual("");
+});
+
+test("can get the next directory up", () => {
+  const currentDir = "topLevel/secondLevel/thirdLevel";
+
+  const result = getNextDirUp(currentDir);
+
+  expect(result).toEqual("topLevel/secondLevel");
+});
+
+test("can get containing directory of file", () => {
+  const file = "topLevel/secondLevel/thirdLevel/testing.txt";
+
+  const result = getNextDirUp(file);
+
+  expect(result).toEqual("topLevel/secondLevel/thirdLevel")
+});
+
+test("next directory up from nothing is nothing", () => {
+  const result = getNextDirUp("");
 
   expect(result).toEqual("");
 });
