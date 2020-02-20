302da25421ceaca95ebca0e91f89482e93bdedc1
My Name <myemail@email.com>
2020-02-20 11:01:45 +0000

    Added comments data structure.

--- tapp/repos/repos.py
+++ tapp/repos/repos.py
@@ -1,4 +1,9 @@
+from collections import defaultdict
+from typing import Dict, List
+
 from flask import Blueprint, jsonify, abort, send_from_directory
+
+from ..dbcr.comments import Comment
 
 repos_bp = Blueprint("repos", __name__, url_prefix="/api/v1.0/repos", static_folder="static")
 
@@ -19,6 +24,25 @@ repos = {
         }
     }
 }
+
+# Track repo id and file path. Store mapping from line numbers to chronologically ordered
+# comments.
+
+comments: Dict[str, Dict[int, List[Comment]]] = defaultdict(lambda: defaultdict(lambda: []))
+comment_id = 0
+
+def get_comment_key(repo_id: int, comment: Comment) -> str:
+    return str(repo_id) + ":" + comment.get_file_path()
+
+
+def add_comment(repo_id: int, comment: Comment):
+    key = get_comment_key(repo_id, comment)
+
+    global comments
+    global comment_id
+
+    comments[key][comment.get_line_number()] = comment
+    comment_id += 1
 
 
 # Return tuple of path and filename. Path will not end with a slash
@@ -45,6 +69,10 @@ def flatten_directories(dir_contents):
 
     return flattened
 
+def check_repo(repo_id: str):
+    if not repo_id in repos:
+        abort(404)
+
 @repos_bp.route("/banter/<string:name>")
 def reply(name):
     return "The banter is with you" + name
@@ -52,8 +80,7 @@ def reply(name):
 @repos_bp.route("/view/dir/<string:repo_id>/", defaults={"path": ""}, methods=["GET"])
 @repos_bp.route("/view/dir/<string:repo_id>/<path:path>", methods=["GET"])
 def get_repo(repo_id: str, path: str):
-    if not repo_id in repos:
-        abort(404)
+    check_repo(repo_id)
 
     dir_contents = repos[repo_id]
 
@@ -72,11 +99,25 @@ def get_repo(repo_id: str, path: str):
 
 @repos_bp.route("/view/file/<string:repo_id>/<path:path>", methods=["GET"])
 def get_file(repo_id: str, path: str):
-    if not repo_id in repos:
-        abort(404)
+    check_repo(repo_id)
 
     file_path, file_name = splitFilePath(path)
 
     full_file_path = repos_bp.static_folder + "/" + repo_id + "/" + file_path
 
     return send_from_directory(full_file_path, file_name)
+
+# Comments - TODO extract elsewhere
+
+@repos_bp.route("/view/comments/<string:repo_id>/<path:path>", methods=["GET"])
+def get_comments(repo_id: str, path: str):
+    check_repo(repo_id)
+    pass
+
+
+@repos_bp.route("/comment/<string:repo_id>/<string:user_id>/<string:comment>/<int:line_number>/<string:parent_id>/<path:file_path>", methods=["POST"])
+def post_comment(repo_id: str, user_id: str, comment: str, line_number: int, parent_id: str, file_path: str):
+    check_repo(repo_id)
+
+    add_comment(repo_id, Comment())
+    pass
