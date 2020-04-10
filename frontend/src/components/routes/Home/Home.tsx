e6052452b5c2dce6aa2aaa3bc299fbf86731552d
My Name <myemail@email.com>
2020-04-10 11:10:01 +0100

    Added basic dashboard page structure.

--- frontend/src/components/routes/Home/Home.tsx
+++ frontend/src/components/routes/Home/Home.tsx
@@ -3,24 +3,31 @@ import useStyles from "./style"
 import ViewRepos from "../ViewRepos/ViewRepos";
 import { Typography } from "antd";
 import NewRepo from "../ViewRepos/NewRepo";
+import { useParams } from "react-router-dom";
 // import coding from "../../../assets/coding.gif"
 // import Grid from "@material-ui/core/Grid/index"
 
 const { Title } = Typography;
 
+// TODO - get username
 const Home = () => {
-  const classes = useStyles();
+  const {user} = useParams();
 
   return (
     <div>
+      <Title underline level={1}>{user}</Title>
+
+      <Title underline level={2}>Your repos:</Title>
       <Title level={2}>Create Repository:</Title>
       <NewRepo/>
-
-      <Title level={2}>Available repositories:</Title>
-        
+      <br/>
       <ViewRepos/>
+
+      <Title underline level={2}>Your reviews:</Title>
+
+      <Title underline level={2}>Your reviewer pools</Title>
     </div>
   )
-}
+};
 
 export default Home
