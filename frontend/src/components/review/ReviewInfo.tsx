d8fb5dfd484915234a44d18af2fbae4aa4a63b75
My Name <myemail@email.com>
2020-04-10 17:26:31 +0100

    Built initial review info component.

--- frontend/src/components/review/ReviewInfo.tsx
+++ frontend/src/components/review/ReviewInfo.tsx
@@ -1,12 +1,26 @@
 import * as React from "react"
+import { ReviewStats } from "../../types/ReviewStats";
+import { Badge, Button, Tag } from "antd";
 
 interface Props {
-  url: string,
-
+  reviewUrl: string,
+  reviewStats: ReviewStats
 }
 
-const ReviewInfo = () => {
+const ReviewInfo = (props: Props) => {
+  const {reviewUrl, reviewStats} = props;
+  const approvalsMessage = "" + reviewStats.approvals + " approvals";
+  const rejectionsMessage = "" + reviewStats.rejections + " rejections";
 
+  return <>
+    <Button type={"primary"}
+            href={reviewUrl}>
+      {reviewStats.repoName} | {reviewStats.branch}
+    </Button>
+    <Badge count={reviewStats.newComments}/>
+    <Tag color={reviewStats.approvals ? "green" : undefined}>{approvalsMessage}</Tag>
+    <Tag color={reviewStats.rejections ? "red" : undefined}>{rejectionsMessage}</Tag>
+  </>
 };
 
 export default ReviewInfo;
