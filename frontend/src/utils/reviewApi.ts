e0460878246beddd0c11d02a9f19473eff06d525
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-04-10 17:44:20 +0100

    Added placeholder review submissions to UI.

--- frontend/src/utils/reviewApi.ts
+++ frontend/src/utils/reviewApi.ts
@@ -1,6 +1,6 @@
 import reviewInfos from "../data/reviewInfos.json";
 import { ReviewStats } from "../types/ReviewStats";
 
-export async function getReviewInfos(): Promise<ReviewStats[]> {
+export async function getReviewStats(): Promise<ReviewStats[]> {
   return reviewInfos;
 }
