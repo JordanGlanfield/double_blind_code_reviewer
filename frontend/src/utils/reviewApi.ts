79176343b8971cb3f454a38d415607984b95e840
My Name <myemail@email.com>
2020-04-12 09:14:59 +0100

    Added placeholder area for reviews requested from you on dashboard.

--- frontend/src/utils/reviewApi.ts
+++ frontend/src/utils/reviewApi.ts
@@ -1,5 +1,10 @@
+import reviewSubmissionInfos from "../data/reviewSubmissionInfos.json";
 import reviewInfos from "../data/reviewInfos.json";
 import { ReviewStats } from "../types/ReviewStats";
+
+export async function getReviewSubmissionStats(): Promise<ReviewStats[]> {
+  return reviewSubmissionInfos;
+}
 
 export async function getReviewStats(): Promise<ReviewStats[]> {
   return reviewInfos;
