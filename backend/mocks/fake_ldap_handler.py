827ce0d61076825c8327931d472138a820183a0a
My Name <myemail@email.com>
2020-04-12 16:50:56 +0100

    Small DB model fixes and added initial DB migrations. Login now uses User table.

--- backend/mocks/fake_ldap_handler.py
+++ backend/mocks/fake_ldap_handler.py
@@ -1,5 +1,7 @@
 import ldap
 
+from ..db.database import DB
+from ..db.models import *
 from ..utils import file_utils
 
 
@@ -10,13 +12,17 @@ class FakeLdapConnectionHandler:
     def init_app(self, *_):
         pass
 
+    def write_test_users(self):
+        test_users: {} = file_utils.read_json_file("backend/mocks/fake_ldap_base/users.json")
+        for username in test_users:
+            if not User.query.filter_by(username=username).first():
+                DB.add(User(username=username))
+
     @staticmethod
     def ldap_login(username, *args, **kwargs):
-        test_users = file_utils.read_json_file("backend/mocks/fake_ldap_base/users.json")
-        if username not in test_users:
+        if not User.query.filter_by(username=username).first():
             raise ldap.INVALID_CREDENTIALS
         print("[LDAP] Logging in as '%s'" % username)
-        return test_users.get(username)
-
+        return ()
 
 FAKE_LDAP = FakeLdapConnectionHandler()
