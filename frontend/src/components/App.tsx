91c1c12fe074eac794718ad659a35dad55d2cc00
My Name <myemail@email.com>
2020-04-27 14:33:52 +0100

    Massively simplified routing.

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -24,96 +24,39 @@ const App = () => {
   const isAuthenticatedSource = useDataSource(checkIsAuthenticated);
   const isLoggedIn = !isAuthenticatedSource.isFetching && isAuthenticatedSource.data;
 
-  useEffect(() => {console.log("TopBar Mounted Again"); return () => console.log("TopBar unmounted")}, [])
+  // useEffect(() => {console.log("TopBar Mounted Again"); return () => console.log("TopBar unmounted")}, [])
 
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Switch>
-          <DefaultPageRoute exact
-                            path={routes.SIGNUP}
-                            isLoggedIn={isLoggedIn}
-                            content={Signup}
-                            contentProps={{loggedIn: isAuthenticatedSource.forceRefetch}}/>
-          <DefaultPageRoute exact
-                            path={routes.LOGIN}
-                            isLoggedIn={isLoggedIn}
-                            content={Login}
-                            contentProps={{loggedIn: isAuthenticatedSource.forceRefetch}} />
-          {!isLoggedIn && <DefaultPageRoute isLoggedIn={false} content={Empty} />}
-          <DefaultPageRoute path={routes.HOME} isLoggedIn={isLoggedIn} content={Home} />
-          <DefaultPageRoute path={routes.REPO_DIRS} isLoggedIn={isLoggedIn} content={RepoDir} />
-          <DefaultPageRoute path={routes.REPO_FILES}
-                            isLoggedIn={isLoggedIn}
-                            content={RepoFile} />
-          <DefaultPageRoute path={routes.REVIEWER_POOL}
-                            isLoggedIn={isLoggedIn}
-                            content={ReviewerPoolDashboard} />
-          <DefaultPageRoute path={routes.CREATE_REPO}
-                            isLoggedIn={isLoggedIn}
-                            content={CreateRepo} />
-          <DefaultPageRoute isLoggedIn={isLoggedIn} content={NotFound} />
-        </Switch>
+      <BrowserRouter forceRefresh={false}>
+        <Layout>
+          <Layout.Header>
+            <TopBar isLoggedIn={isLoggedIn}/>
+          </Layout.Header>
+          <Layout.Content>
+            <ContentDiv>
+              <Switch>
+                <Route exact path={routes.SIGNUP} component={Signup} />
+                <Route exact path={routes.LOGIN}
+                       render={props =>
+                         <Login {...props} isLoggedIn={isLoggedIn} loggedIn={isAuthenticatedSource.forceRefetch} />} />
+                {!isLoggedIn && <Route render={props => <div>Not authenticated</div>}/>}
+                <Route path={routes.HOME} component={Home} />
+                <Route path={routes.REPO_DIRS} component={RepoDir} />
+                <Route path={routes.REPO_FILES} component={RepoFile} />
+                <Route path={routes.REVIEWER_POOL} component={ReviewerPoolDashboard} />
+                <Route path={routes.CREATE_REPO} component={CreateRepo} />
+                <Route component={NotFound} />
+              </Switch>
+            </ContentDiv>
+          </Layout.Content>
+          <Layout.Footer>
+            <Empty />
+          </Layout.Footer>
+        </Layout>
       </BrowserRouter>
     </ThemeProvider>
   )
-};
-
-interface RouteWrapperProps {
-  component: (props: any) => JSX.Element;
-  layout: (props: any) => JSX.Element;
-}
-
-const RouteWrapper = ({component: Component, layout: Layout, ...rest}: RouteWrapperProps) => {
-  return <Route {...rest} render={props =>
-    <Layout {...props}>
-      <Component {...props} />
-    </Layout>
-  }/>
-};
-
-interface DefaultPageRouteProps {
-  isLoggedIn: boolean;
-  content: (props: any) => JSX.Element;
-  contentProps?: any;
-}
-
-const DefaultPageRoute = ({path, isLoggedIn, content: Content, contentProps, ...rest}
-    : DefaultPageRouteProps & RouteProps) => {
-  return <PageRoute
-    {...rest}
-    header={TopBar}
-    headerProps={{isLoggedIn: isLoggedIn}}
-    content={Content}
-    contentProps={contentProps}
-    footer={Empty}
-  />
-};
-
-interface PageRouteProps {
-  header: (props: any) => JSX.Element;
-  headerProps?: any;
-  content: (props: any) => JSX.Element;
-  contentProps?: any;
-  footer: (props: any) => JSX.Element;
-  footerProps?: any;
-}
-
-const PageRoute = ({header: Header, headerProps, content: Content, contentProps, footer: Footer, footerProps, ...rest}
-    : PageRouteProps & RouteProps) => {
-  return <Route {...rest} render={props =>
-    <Layout>
-      <Layout.Header>
-        <Header {...props} {...headerProps} />
-      </Layout.Header>
-      <Layout.Content>
-        <Content {...props} {...contentProps} />
-      </Layout.Content>
-      <Layout.Footer>
-        <Footer {...props} {...footerProps} />
-      </Layout.Footer>
-    </Layout>
-  }/>
 };
 
 const Empty = () => {
